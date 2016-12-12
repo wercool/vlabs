dd988424b37a01370b07f51a47d292906f54b850
maska <maska@sashimi>
2016-12-12 20:16:18 +0200

    initial JS structure in progress

--- models/ph-mpd-fcm/prod/js/vlab.js
+++ models/ph-mpd-fcm/prod/js/vlab.js
@@ -1,13 +1,11 @@
 "use strict";
 
-function VLab(constructorArgs)
+function VLab(vlabNature)
 {
     var self = this;
     var logging = true;
 
-    var title            = constructorArgs.title;
-    var sceneFile        = constructorArgs.sceneFile;
-    var showAxis         = constructorArgs.showAxis;
+    self.vlabNature = vlabNature;
 
     // DOM container element
     var webglContainerDOM = null;
@@ -19,7 +17,8 @@ function VLab(constructorArgs)
 
     self.WebGLRenderer = null;
 
-    var sceneLoadedEvent = new Event("sceneLoaded");
+    var sceneLoadedEvent    = new Event("sceneLoaded");
+    var simulationStepEvent = new Event("simulationStep");
 
     var vlabScene = null;
     var vlabPhysijsSceneReady = false;
@@ -29,17 +28,33 @@ function VLab(constructorArgs)
     var meshObjects = new Object();
     var lights = [];
 
+    var mouseCoords = new THREE.Vector2();
+    var raycaster = new THREE.Raycaster();
+    var mouseDownEvent = null;
+    var mouseUpEvent = null;
+
     self.clickResponsiveObjects = [];
     self.hoverResponsiveObjects = [];
 
-    self.clickedObjects = [];
-    self.hoveredObjects = [];
+    var intersectedMesh     = null;
+    var intersectedMeshName = "";
+    var notStrictedReleaseCallbacks = [];
 
-    self.trace = function()
+    var tooltipDiv = null;
+
+    self.trace = function(error)
     {
         if (logging)
         {
             console.log.apply(console, arguments);
+        }
+    };
+
+    self.error = function()
+    {
+        if (logging)
+        {
+            console.error.apply(console, arguments);
         }
     };
 
@@ -85,7 +100,10 @@ function VLab(constructorArgs)
 
         $(window).on("resize", webglContainerResized);
 
-        loadScene(sceneFile);
+        self.webglContainerWidth  = webglContainer.width();
+        self.webglContainerHeight = webglContainer.height();
+
+        loadScene(self.vlabNature.sceneFile);
     };
 
     var loadScene = function(sceneFile)
@@ -155,6 +173,29 @@ function VLab(constructorArgs)
 
     this.buildScene = function()
     {
+        if (self.vlabNature.isPhysijsScene)
+        {
+            Physijs.scripts.worker = "/js/physijs_worker.js";
+            Physijs.scripts.ammo = "/js/ammo.js";
+            vlabScene = new Physijs.Scene({ fixedTimeStep: 1 / 120});
+            vlabScene.setGravity(new THREE.Vector3( 0, -30, 0 ));
+
+            if (self.vlabNature.isPhysijsScene)
+            {
+                vlabScene.addEventListener(
+                    "update",
+                    function() 
+                    {
+                        self.vlabPhysijsSceneReady = true;
+                    }
+                );
+            }
+        }
+        else
+        {
+            vlabScene = new THREE.Scene();
+        }
+
         // add children meshes to root meshes
         for (var meshObjectName in meshObjects)
         {
@@ -171,7 +212,70 @@ function VLab(constructorArgs)
         {
             if(meshObjects[meshObjectName].isRoot)
             {
-                vlabScene.add(meshObjects[meshObjectName].mesh);
+                if (self.vlabNature.physijs[meshObjectName] != undefined)
+                {
+                    var physijsMesh;
+                    var position = new THREE.Vector3();
+                    var quaternion = new THREE.Quaternion();
+                    quaternion.copy(meshObjects[meshObjectName].mesh.quaternion);
+                    position.copy(meshObjects[meshObjectName].mesh.position);
+
+                    var physijsMaterial = Physijs.createMaterial(
+                        meshObjects[meshObjectName].mesh.material,
+                        self.vlabNature.physijs[meshObjectName].friction,
+                        self.vlabNature.physijs[meshObjectName].restitution
+                    );
+
+                    switch (self.vlabNature.physijs[meshObjectName].shape)
+                    {
+                        case "BoxMesh":
+                            physijsMesh = new Physijs.BoxMesh(
+                                                    meshObjects[meshObjectName].mesh.geometry,
+                                                    physijsMaterial
+                                                );
+                        break;
+                        case "ConvexMesh":
+                            physijsMesh = new Physijs.ConvexMesh(
+                                                    meshObjects[meshObjectName].mesh.geometry,
+                                                    physijsMaterial
+                                                );
+                        break;
+                    }
+                    physijsMesh.quaternion.copy(quaternion);
+                    physijsMesh.position.copy(position);
+                    physijsMesh.name = meshObjectName;
+                    physijsMesh.mass = self.vlabNature.physijs[meshObjectName].mass;
+
+                    // add child meshesh to newly created Physijs Mesh
+                    if(meshObjects[meshObjectName].childMeshes.length > 0)
+                    {
+                        for (var key in meshObjects[meshObjectName].childMeshes)
+                        {
+                            physijsMesh.add(meshObjects[meshObjects[meshObjectName].childMeshes[key]].mesh);
+                        }
+                    }
+
+                    if (self.vlabNature.physijs[meshObjectName].collision != undefined)
+                    {
+                        var collisionCallback = eval("self." + self.vlabNature.physijs[meshObjectName].collision);
+                        if (typeof collisionCallback === "function")
+                        {
+                            physijsMesh.addEventListener("collision", collisionCallback);
+                        }
+                        else
+                        {
+                            self.error("Collision callback [" + 
+                                        self.vlabNature.physijs[meshObjectName].collision + 
+                                        "] for the [" + meshObjectName + "] mesh is defined in VLab nature, but not implemented in VLab");
+                        }
+                    }
+
+                    vlabScene.add(physijsMesh);
+                }
+                else
+                {
+                    vlabScene.add(meshObjects[meshObjectName].mesh);
+                }
             }
         }
         // add lights
@@ -180,12 +284,81 @@ function VLab(constructorArgs)
             vlabScene.add(lights[i]);
         }
 
-        if (showAxis)
+        if (self.vlabNature.showAxis)
         {
             var axisHelper = new THREE.AxisHelper(100.0);
             vlabScene.add(axisHelper);
         }
-console.log(vlabScene);
+
+        if (self.vlabNature.showTooltips)
+        {
+            //tooltip
+            tooltipDiv = $("<div id='tooltipDiv' class='tooltip'></div>");
+            webglContainer.append(tooltipDiv);
+            tooltipDiv.hide();
+        }
+
+        vlabScene.traverse (function (object)
+        {
+            if (object.type == "Mesh")
+            {
+                if (self.vlabNature.interactors[object.name] != undefined)
+                {
+                    object.active = self.vlabNature.interactors[object.name].active;
+                    if (self.vlabNature.interactors[object.name].tooltip != undefined)
+                    {
+                        object.tooltip = self.vlabNature.interactors[object.name].tooltip;
+                    }
+                    self.hoverResponsiveObjects.push(object);
+                    if (object.active)
+                    {
+                        self.clickResponsiveObjects.push(object);
+                        if (self.vlabNature.interactors[object.name].press != undefined)
+                        {
+                            var objectMousePressCallback = eval("self." + self.vlabNature.interactors[object.name].press);
+                            if (typeof objectMousePressCallback === "function")
+                            {
+                                object.press = objectMousePressCallback;
+                            }
+                            else
+                            {
+                                self.error("MousePress callback [" + 
+                                            self.vlabNature.interactors[object.name].press + 
+                                            "] for [" + object.name + "] mesh is defined in VLab nature, but not implemented in VLab");
+                            }
+                        }
+                        if (self.vlabNature.interactors[object.name].release != undefined)
+                        {
+                            var objectMouseReleaseCallback = eval("self." + self.vlabNature.interactors[object.name].release.callbak);
+                            if (typeof objectMouseReleaseCallback === "function")
+                            {
+                                object.release = objectMouseReleaseCallback;
+                                if (self.vlabNature.interactors[object.name].release.strict != undefined)
+                                {
+                                    object.strictRelease = self.vlabNature.interactors[object.name].release.strict;
+                                }
+                                else
+                                {
+                                    object.strictRelease = false;
+                                }
+                            }
+                            else
+                            {
+                                self.error("MouseRelease callback [" + 
+                                            self.vlabNature.interactors[object.name].release.callbak + 
+                                            "] for [" + object.name + "] mesh is defined in VLab nature, but not implemented in VLab");
+                            }
+                        }
+                    }
+                }
+            }
+        });
+
+        if (self.vlabNature.isPhysijsScene)
+        {
+            vlabScene.simulate();
+        }
+
         render();
     }
 
@@ -193,12 +366,34 @@ console.log(vlabScene);
     {
         self.WebGLRenderer.render(vlabScene, defaultCamera);
         requestAnimationFrame(render);
+        if ((self.vlabNature.isPhysijsScene && self.vlabPhysijsSceneReady) || !self.vlabNature.isPhysijsScene)
+        {
+            process();
+            if (self.vlabNature.isPhysijsScene)
+            {
+                vlabScene.simulate(undefined, 1);
+                self.vlabPhysijsSceneReady = false;
+            }
+        }
     };
+
+    var process = function()
+    {
+        if (mouseDownEvent != null)
+        {
+            processMouseDown();
+        }
+        if (mouseUpEvent != null)
+        {
+            processMouseUp();
+        }
+        dispatchEvent(simulationStepEvent);
+    }
 
     self.setVlabScene = function(scene)
     {
         vlabScene = scene;
-        if (vlabScene.isPhysijsScene)
+        if (self.vlabNature.isPhysijsScene)
         {
             vlabScene.addEventListener(
                 "update",
@@ -224,21 +419,143 @@ console.log(vlabScene);
 
     var mouseMove = function(event)
     {
-        self.trace("mouseMove");
+        event.preventDefault();
+        mouseCoords.set((event.clientX / webglContainer.width()) * 2 - 1, 1 -(event.clientY / webglContainer.height()) * 2);
+        raycaster.setFromCamera(mouseCoords, defaultCamera);
+        var intersects = raycaster.intersectObjects(self.hoverResponsiveObjects); 
+
+        if (intersects.length > 0) 
+        {
+            if (self.intersectedMeshName != intersects[0].object.name)
+            {
+                self.instersectedMesh = intersects[0].object;
+                self.intersectedMeshName = self.instersectedMesh.name;
+                if (self.instersectedMesh.active)
+                {
+                    webglContainerDOM.style.cursor = 'pointer';
+                }
+
+                if (self.vlabNature.showTooltips)
+                {
+                    if (self.instersectedMesh.tooltip != undefined)
+                    {
+                        var tooltipPosition = toScreenPosition(self.instersectedMesh);
+                        tooltipDiv.text(self.instersectedMesh.tooltip);
+                        tooltipDiv.show();
+                        tooltipDiv.css({left: tooltipPosition.x + 10, top: tooltipPosition.y - 30});
+                    }
+                }
+            }
+        }
+        else
+        {
+            webglContainerDOM.style.cursor = 'auto';
+
+            if (self.intersectedMeshName != null)
+            {
+                self.intersectedMesh = null;
+                self.intersectedMeshName = null;
+
+                if (self.vlabNature.showTooltips)
+                {
+                    tooltipDiv.text("");
+                    tooltipDiv.hide();
+                }
+            }
+        }
     };
 
     var mouseDown = function(event)
     {
-        self.trace("mouseDown");
+        event.preventDefault();
+        if (mouseDownEvent == null)
+        {
+            mouseCoords.set((event.clientX / webglContainer.width()) * 2 - 1, 1 -(event.clientY / webglContainer.height()) * 2);
+            mouseDownEvent = event;
+        }
     };
 
     var mouseUp = function(event)
     {
-        self.trace("mouseUp");
+        event.preventDefault();
+        if (mouseUpEvent == null)
+        {
+            mouseCoords.set((event.clientX / webglContainer.width()) * 2 - 1, 1 -(event.clientY / webglContainer.height()) * 2);
+            mouseUpEvent = event;
+        }
     };
 
     self.getWebglContainerDOM = function(){return webglContainerDOM};
     self.getWebglContainer = function(){return webglContainer};
     self.getVlabScene = function(){return vlabScene};
     self.getDefaultCamera = function(){return defaultCamera};
+
+    var processMouseDown = function()
+    {
+        if (mouseDownEvent != null)
+        {
+            raycaster.setFromCamera(mouseCoords, defaultCamera);
+            var intersects = raycaster.intersectObjects(self.clickResponsiveObjects);
+            if (intersects.length > 0) 
+            {
+                var pressedObject = intersects[0].object;
+
+                if (typeof pressedObject.press === "function")
+                {
+                    pressedObject.press();
+                }
+                if (typeof pressedObject.release === "function" && !pressedObject.strictRelease)
+                {
+                    notStrictedReleaseCallbacks.push(pressedObject.release);
+                }
+            }
+            mouseDownEvent = null;
+        }
+    };
+
+    var processMouseUp = function()
+    {
+        if (mouseUpEvent != null)
+        {
+            raycaster.setFromCamera(mouseCoords, defaultCamera);
+            var intersects = raycaster.intersectObjects(self.clickResponsiveObjects);
+            if (intersects.length > 0) 
+            {
+                var releasedObject = intersects[0].object;
+
+                if (typeof releasedObject.release === "function")
+                {
+                    releasedObject.release();
+                }
+            }
+            var i = notStrictedReleaseCallbacks.length;
+            while (i--)
+            {
+                var releaseCallback = notStrictedReleaseCallbacks.pop();
+                console.log(releaseCallback);
+                releaseCallback();
+            }
+            mouseUpEvent = null;
+        }
+    };
+
+    var toScreenPosition = function(obj)
+    {
+        var vector = new THREE.Vector3();
+
+        var widthHalf = 0.5 * self.webglContainerWidth;
+        var heightHalf = 0.5 * self.webglContainerHeight;
+
+        obj.updateMatrixWorld();
+        vector.setFromMatrixPosition(obj.matrixWorld);
+        vector.project(defaultCamera);
+
+        vector.x = ( vector.x * widthHalf ) + widthHalf;
+        vector.y = - ( vector.y * heightHalf ) + heightHalf;
+
+        return { 
+            x: vector.x,
+            y: vector.y
+        };
+    };
 };
--- models/ph-mpd-fcm/prod/ph-mpd-fcm.js
+++ models/ph-mpd-fcm/prod/ph-mpd-fcm.js
@@ -4,30 +4,50 @@ function PhMpdFcm(webGLContainer)
 {
     var self = this;
 
-    VLab.apply(self, [{title: "Friction coefficient measurement",
-                       sceneFile: "ph-mpd-fcm.dae",
-                       showAxis: true}]);
-
-    self.initialize(webGLContainer);
-
     addEventListener("sceneLoaded", function (event) { sceneLoaded(); }, false);
+    addEventListener("simulationStep", function (event) { simulationStep(); }, false);
+
+    $.getJSON("ph-mpd-fcm.json", function(jsonObj) {
+        VLab.apply(self, [jsonObj]);
+        self.initialize(webGLContainer);
+    });
 
     var sceneLoaded = function()
     {
-        self.getDefaultCamera().position.set(0.0, 0.0, 15.0);
+        self.getDefaultCamera().position.set(0.0, 4.0, 14.0);
 
         self.getDefaultCamera().controls = new THREE.OrbitControls(self.getDefaultCamera(), self.getWebglContainerDOM());
         self.getDefaultCamera().controls.minDistance = 5;
         self.getDefaultCamera().controls.maxDistance = 15;
+        self.getDefaultCamera().controls.maxPolarAngle = Math.PI/2 - 0.2; 
+        self.getDefaultCamera().controls.minPolarAngle = 0.85; 
         self.getDefaultCamera().controls.autoRotate = false;
         self.getDefaultCamera().controls.enableKeys = false;
 
-        Physijs.scripts.worker = "/js/physijs_worker.js";
-        Physijs.scripts.ammo = "/js/ammo.js";
-        var scene = new Physijs.Scene({ fixedTimeStep: 1 / 120});
-        scene.isPhysijsScene = true;
-        self.setVlabScene(scene);
         self.buildScene();
     };
 
+    self.button1Pressed = function()
+    {
+        self.trace(this.name + " is pressed");
+    };
+
+    self.button1Released = function()
+    {
+        self.trace(this.name + " is released");
+    };
+
+    self.slopingBodyCollision = function(other_object, linear_velocity, angular_velocity)
+    {
+        self.trace(this.name + " [collided with] " + other_object.name);
+    };
+
+    self.slopingSurfaceCollision = function(other_object, linear_velocity, angular_velocity)
+    {
+        self.trace(this.name + " [collided with] " + other_object.name);
+    };
+
+    var simulationStep = function()
+    {
+    }
 }
