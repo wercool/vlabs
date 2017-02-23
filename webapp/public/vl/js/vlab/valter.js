db0461eefb18251ebeb730579788790d843300c3
maska <maska@mail.ru>
2017-02-23 19:59:34 +0200

    Valter Lab (dat.GUI added)

--- webapp/public/vl/js/vlab/valter.js
+++ webapp/public/vl/js/vlab/valter.js
@@ -11,9 +11,13 @@ class Valter
         this.testMode = testMode;
         this.vlab.trace("Valter initializing...");
 
+        this.activeObjects = {};
+
         var loader = new THREE.ObjectLoader();
         loader.convertUpAxis = true;
         loader.load(this.valterJSON, this.initialize.bind(this), this.sceneLoading.bind(this));
+
+        addEventListener("simulationStep", this.simulationStep, false);
     }
 
     sceneLoading(bytes)
@@ -34,14 +38,30 @@ class Valter
             var shadowCasted = [
                                 "baseFrame", "base", "rightWheel", "rightWheelDiskBack", "leftWheel", "leftWheelDiskBack",
                                 "manGripperFrame", "valterBodyP1", "valterBodyP2", "bodyFrame", "bodyFrameL", "bodyFrameR",
-                                "pg20RMiddle", "pg20LMiddle"
+                                "pg20RMiddle", "pg20LMiddle", "bodyFrameRFixed", "bodyFrameLFixed", "neckFabrickCover", "headFrame",
+                                "kinectBodyHead", "wifiAntennaLeft", "wifiAntennaCenter", "wifiAntennaRight",
+                                "armfixtureRight", "armRightShoulderAxis", "armCover1R", "armCover2R", "armActuatorP1Right", "armp1mR",
+                                "armCover3R", "armCover4R", "armCover5R", "armCover6R", "forearmCoverR", "rHandCover", "rightPalmLiftFixtureP2", "rightPalmFixtureP14",
+                                "r_0_finger_p1", "r_0_finger_p2", "r_0_finger_p3", "r_0_finger_nail",
+                                "r_1_finger_p1", "r_1_finger_p2", "r_1_finger_p3", "r_1_finger_nail",
+                                "r_2_finger_p1", "r_2_finger_p2", "r_2_finger_p3", "r_2_finger_nail",
+                                "r_3_finger_p1", "r_3_finger_p2", "r_3_finger_p3", "r_3_finger_nail",
+                                "r_4_finger_p1", "r_4_finger_p2", "r_4_finger_p3", "r_4_finger_nail",
+                                "r_5_finger_p1", "r_5_finger_p2", "r_5_finger_p3", "r_5_finger_nail",
+                                "armfixtureLeft", "armLeftShoulderAxis", "armCover1L", "armCover2L", "armActuatorP1Left", "armp1mL",
+                                "armCover3L", "armCover4L", "armCover5L", "armCover6L", "forearmCoverL", "lHandCover", "leftPalmLiftFixtureP2", "leftPalmFixtureP14",
+                                "l_0_finger_p1", "l_0_finger_p2", "l_0_finger_p3", "l_0_finger_nail",
+                                "l_1_finger_p1", "l_1_finger_p2", "l_1_finger_p3", "l_1_finger_nail",
+                                "l_2_finger_p1", "l_2_finger_p2", "l_2_finger_p3", "l_2_finger_nail",
+                                "l_3_finger_p1", "l_3_finger_p2", "l_3_finger_p3", "l_3_finger_nail",
+                                "l_4_finger_p1", "l_4_finger_p2", "l_4_finger_p3", "l_4_finger_nail",
+                                "l_5_finger_p1", "l_5_finger_p2", "l_5_finger_p3", "l_5_finger_nail",
+                                "armAxisBearingR", "armAxisBearingL"
                                ];
            var doubleSide = [
-                               "valterBodyP1", "valterBodyP2", "bodyKinectFrame", "neckFabrickCover", "armCover1", "armCover2",
-                               "armCover3", "armCover4", "armCover5", "armCover6", "armCover7", "armCover8", "armCover9",
-                               "armCover10", "forearmCover",
-                               "armCover1l", "armCover2l", "armCover3l", "armCover4l", "armCover5l", "armCover6l",
-                               "armCover7l", "armCover8l", "armCover8ml", "armCover9l", "armCover10l", "forearmCoverl"
+                               "valterBodyP1", "valterBodyP2", "bodyKinectFrame", "neckFabrickCover",
+                               "armCover1R", "armCover2R", "armCover3R", "armCover4R", "armCover5R", "armCover6R", "forearmCoverR",
+                               "armCover1L", "armCover2L", "armCover3L", "armCover4L", "armCover5L", "armCover6L", "forearmCoverL"
                               ];
             if (shadowCasted.indexOf(obj.name) > -1)
             {
@@ -85,6 +105,34 @@ class Valter
         this.model.position.copy(this.initialModelPosition);
         this.vlab.getVlabScene().add(this.model);
 
+        this.model.updateMatrixWorld();
+
+        this.activeObjects["ValterBase"] = this.vlab.getVlabScene().getObjectByName("ValterBase");
+        this.activeObjects["valterBodyP1"] = this.vlab.getVlabScene().getObjectByName("valterBodyP1");
+        this.activeObjects["bodyFrameAxisR"] = this.vlab.getVlabScene().getObjectByName("bodyFrameAxisR");
+        this.activeObjects["bodyFrameR"] = this.vlab.getVlabScene().getObjectByName("bodyFrameR");
+        this.activeObjects["bodyFrameL"] = this.vlab.getVlabScene().getObjectByName("bodyFrameL");
+
+        this.activeObjects["armRightShoulderAxis"] = this.vlab.getVlabScene().getObjectByName("armRightShoulderAxis");
+        this.activeObjects["armLeftShoulderAxis"] = this.vlab.getVlabScene().getObjectByName("armLeftShoulderAxis");
+        this.activeObjects["rightArm"] = this.vlab.getVlabScene().getObjectByName("rightArm");
+        this.activeObjects["armActuatorP1Right"] = this.vlab.getVlabScene().getObjectByName("armActuatorP1Right");
+        this.activeObjects["leftArm"] = this.vlab.getVlabScene().getObjectByName("leftArm");
+        this.activeObjects["armActuatorP1Left"] = this.vlab.getVlabScene().getObjectByName("armActuatorP1Left");
+
+        this.activeObjects["rightForearmTilt"] = this.vlab.getVlabScene().getObjectByName("rightForearmTilt");
+        this.activeObjects["leftForearmTilt"] = this.vlab.getVlabScene().getObjectByName("leftForearmTilt");
+
+        this.activeObjects["forearmFrameRight"] = this.vlab.getVlabScene().getObjectByName("forearmFrameRight");
+        this.activeObjects["forearmFrameLeft"] = this.vlab.getVlabScene().getObjectByName("forearmFrameLeft");
+
+        this.activeObjects["headTiltFrame"] = this.vlab.getVlabScene().getObjectByName("headTiltFrame");
+        this.activeObjects["headYawFrame"] = this.vlab.getVlabScene().getObjectByName("headYawFrame");
+
+        //initial joint values
+        this.rightArm_initialRotY = this.activeObjects["rightArm"].rotation.y;
+        this.armActuatorP1Right_initialRotX = this.activeObjects["armActuatorP1Right"].rotation.x;
+
         if (this.testMode)
         {
             var control = new THREE.TransformControls(this.vlab.getDefaultCamera(), this.vlab.WebGLRenderer.domElement);
@@ -92,6 +140,49 @@ class Valter
             control.attach(this.model);
             control.setSize(1.0);
             this.vlab.getVlabScene().add(control);
+
+            var GUIcontrols1 = new dat.GUI();
+            console.log("forearmFrameRight", this.activeObjects["forearmFrameRight"].rotation.y);
+            GUIcontrols1.add(this.activeObjects["ValterBase"].rotation, 'z', -6.28, 0.0).name("Base Yaw").step(0.01);
+            GUIcontrols1.add(this.activeObjects["valterBodyP1"].rotation, 'z', -1.57, 1.57).name("Body Yaw").step(0.01);
+            GUIcontrols1.add(this.activeObjects["bodyFrameAxisR"].rotation, 'x', -0.8, 0.0).name("Body Tilt").step(0.01);
+            GUIcontrols1.add(this.activeObjects["bodyFrameR"].rotation, 'z', 0.0, 1.0).name("Right Shoulder").step(0.01);
+            GUIcontrols1.add(this.activeObjects["bodyFrameL"].rotation, 'z', -1.0, 0.0).name("Left Shoulder").step(0.01);
+            GUIcontrols1.add(this.activeObjects["armRightShoulderAxis"].rotation, 'x', -0.85, 1.4).name("Right Limb").step(0.01);
+            GUIcontrols1.add(this.activeObjects["armLeftShoulderAxis"].rotation, 'x', -0.85, 1.4).name("Left Limb").step(0.01);
+            GUIcontrols1.add(this.activeObjects["rightArm"].rotation, 'y', -2.57, -1.22).name("Right Arm").step(0.01).onChange(this.rightArmAnimationHelper.bind(this));
+            GUIcontrols1.add(this.activeObjects["leftArm"].rotation, 'y', -2.57, -1.22).name("Left Arm").step(0.01).onChange(this.leftArmAnimationHelper.bind(this));
+            GUIcontrols1.add(this.activeObjects["rightForearmTilt"].rotation, 'y', -0.85, 0.85).name("Right Forearm Tilt").step(0.01).onChange(this.rightForearmAnimationHelper.bind(this));
+            GUIcontrols1.add(this.activeObjects["leftForearmTilt"].rotation, 'y', -0.85, 0.85).name("Left Forearm Tilt").step(0.01).onChange(this.leftForearmAnimationHelper.bind(this));
+            GUIcontrols1.add(this.activeObjects["forearmFrameRight"].rotation, 'y', -3.14, 0.0).name("Right Forearm Roll").step(0.01);
+            GUIcontrols1.add(this.activeObjects["forearmFrameLeft"].rotation, 'y', -3.14, 0.0).name("Left Forearm Roll").step(0.01);
+            GUIcontrols1.add(this.activeObjects["headTiltFrame"].rotation, 'x', -2.8, -1.8).name("Head Tilt").step(0.01);
+            GUIcontrols1.add(this.activeObjects["headYawFrame"].rotation, 'z', -4.71, -1.57).name("Head Yaw").step(0.01);
         }
+    }
+
+    simulationStep(event)
+    {
+
+    }
+
+    rightArmAnimationHelper()
+    {
+        this.activeObjects["armActuatorP1Right"].rotation.x = this.armActuatorP1Right_initialRotX + (this.rightArm_initialRotY - this.activeObjects["rightArm"].rotation.y);
+    }
+
+    leftArmAnimationHelper()
+    {
+        this.activeObjects["armActuatorP1Left"].rotation.x = this.armActuatorP1Right_initialRotX + (this.rightArm_initialRotY - this.activeObjects["leftArm"].rotation.y);
+    }
+
+    rightForearmAnimationHelper()
+    {
+
+    }
+
+    leftForearmAnimationHelper()
+    {
+
     }
 }
