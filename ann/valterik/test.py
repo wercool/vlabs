8fd381f89ea118fa8da7763c6989f86df8136f44
maska <maska@mail.ru>
2017-08-17 16:39:46 +0300

    tensorflow experiments

--- ann/valterik/test.py
+++ ann/valterik/test.py
@@ -1,35 +1,85 @@
 import os
 import tensorflow as tf
+import numpy as np
 from valterikdb import ValterIKDB
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
+# from tensorflow.examples.tutorials.mnist import input_data
+# mnist = input_data.read_data_sets('/tmp/tensorflow/mnist/input_data', one_hot=True)
+# x  = mnist.test.images
+# y_ = mnist.test.labels
+# print y_
+# quit()
+
 valterIKDB = ValterIKDB('')
 valterIKDB.dbConnect()
-valterIKDB.getBounds()
 
-# normalizedInput = valterIKDB.getNormalizedInput(54750)
-# print normalizedInput
+#valterIKDB.getBounds()
+eefXMin = -7.510000
+eefXMax = 12.743000
+eefYMin = -8.292000
+eefYMax = 12.649000
+eefZMin = 7.101000
+eefZMax = 20.797000
+valterIKDB.setBounds((eefXMin, eefXMax, eefYMin, eefYMax, eefZMin, eefZMax))
+valterIKDB.printBounds()
 
-normalizedBatch = valterIKDB.getBatch(1000)
 
-for sample in normalizedBatch:
-    print sample
+# Create the model
+x = tf.placeholder(tf.float32, [None, 3])
+W = tf.Variable(tf.zeros([3, 6]))
+b = tf.Variable(tf.zeros([6]))
+y = tf.matmul(x, W) + b
 
+# Define loss and optimizer
+y_ = tf.placeholder(tf.float32, [None, 6])
 
-# W = tf.Variable([.3], dtype=tf.float32)
-# b = tf.Variable([-.3], dtype=tf.float32)
-# x = tf.placeholder(tf.float32)
-# y = tf.placeholder(tf.float32)
+# The raw formulation of cross-entropy,
+#
+#   tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(tf.nn.softmax(y)), reduction_indices=[1]))
+#
+# can be numerically unstable.
+#
+# So here we use tf.nn.softmax_cross_entropy_with_logits on the raw
+# outputs of 'y', and then average across the batch.
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
 
-# sess = tf.Session()
-# init = tf.global_variables_initializer()
-# sess.run(init)
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
 
-# linear_model = W * x + b
+sess = tf.InteractiveSession()
+tf.global_variables_initializer().run()
 
-# squared_deltas = tf.square(linear_model - y)
+trainStep = 1
 
-# loss = tf.reduce_sum(squared_deltas)
+# Train
+for _ in range(10000):
+    batchSize = 100
+    normalizedBatch = valterIKDB.getBatch(batchSize)
 
-# print(sess.run(loss, {x:[1,2,3,4], y:[0,-1,-2,-3]}))
+    batch_xs = np.zeros(shape=(batchSize, 3))
+    batch_ys = np.zeros(shape=(batchSize, 6))
+    batchId = 0
+    for batch in normalizedBatch:
+        batch_xs[batchId] = [batch[1], batch[2], batch[3]]
+        batch_ys[batchId] = [batch[4], batch[5], batch[6], batch[7], batch[8], batch[9]]
+        batchId = batchId + 1
+
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+    # Test trained model
+    batchSize = 10000
+    normalizedTestBatch = valterIKDB.getBatch(batchSize)
+    testBatch_xs = np.zeros(shape=(batchSize, 3))
+    testBatch_ys = np.zeros(shape=(batchSize, 6))
+    testBatchId = 0
+    for testBatch in normalizedTestBatch:
+        testBatch_xs[testBatchId] = [testBatch[1], testBatch[2], testBatch[3]]
+        testBatch_ys[testBatchId] = [testBatch[4], testBatch[5], testBatch[6], testBatch[7], testBatch[8], testBatch[9]]
+        testBatchId = testBatchId + 1
+
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    print("Step %d, Accuracy: %f" % (trainStep, sess.run(accuracy, feed_dict={x: testBatch_xs, y_: testBatch_ys})))
+    trainStep = trainStep + 1
+
--- ann/valterik/valterikdb.py
+++ ann/valterik/valterikdb.py
@@ -12,16 +12,16 @@ class ValterIKDB:
         return getattr(self.instance, name)
 
     class __ValterIKDB:
-        db = None
-        cur = None # Cursor object. It will let execute all the queries you need
-        eefXMin = None
-        eefXMax = None
-        eefYMin = None
-        eefYMax = None
-        eefZMin = None
-        eefZMax = None
         def __init__(self, arg):
             self.val = arg
+            self.db = None
+            self.cur = None # Cursor object. It will let execute all the queries you need
+            self.eefXMin = None
+            self.eefXMax = None
+            self.eefYMin = None
+            self.eefYMax = None
+            self.eefZMin = None
+            self.eefZMax = None
         def __str__(self):
             return repr(self) + self.val
         def dbConnect(self):
@@ -32,15 +32,14 @@ class ValterIKDB:
         def query(self, queryStr):
             self.cur.execute(queryStr)
             return self.cur.fetchall()
-
-        def getBounds(self):
-            result = self.query("SELECT MIN(eefX), MAX(eefX), MIN(eefY), MAX(eefY), MIN(eefZ), MAX(eefZ) FROM rightArm")
-            self.eefXMin = result[0][0]
-            self.eefXMax = result[0][1]
-            self.eefYMin = result[0][2]
-            self.eefYMax = result[0][3]
-            self.eefZMin = result[0][4]
-            self.eefZMax = result[0][5]
+        def setBounds(self, boudns):
+            self.eefXMin = boudns[0]
+            self.eefXMax = boudns[1]
+            self.eefYMin = boudns[2]
+            self.eefYMax = boudns[3]
+            self.eefZMin = boudns[4]
+            self.eefZMax = boudns[5]
+        def printBounds(self):
             print "EEF coordinate bounds"
             print "eefX [%f, %f]" % (self.eefXMin, self.eefXMax)
             print "eefY [%f, %f]" % (self.eefYMin, self.eefYMax)
